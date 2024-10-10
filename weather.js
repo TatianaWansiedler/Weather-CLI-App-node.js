05f6f9673fb51c440dd7ff06b864fae17e8f4487
Tatiana Wansiedler <tatjana@wansiedler.com>
2024-10-10 17:54:41 +0200

    add helper for arguments parsing

---
+++ weather.js
@@ -0,0 +1,22 @@
+#!/user/bin/env node
+
+import { getArgs } from "./helpers/args.js";
+
+const initCLI = () => {
+    const args = getArgs(process.argv);
+    console.log(args);
+
+    if (args.h) {
+        // return help
+    }
+    if (args.s) {
+        // save city
+    }
+    if (args.t) {
+        // save token
+    }
+    // return weather 
+    
+};
+
+initCLI();
